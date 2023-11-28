f9188fc1bfca53071852152028a453e4dbc5aefa
Jayesh Soni <Jayuksoni@gmail.com>
2023-11-28 22:33:34 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,7 +5,7 @@ from chromadb.config import Settings
 load_dotenv()
 
 # Define the folder for storing database
-PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY')
+PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY') # It will create DB folder in which embedding will get saved 
 if PERSIST_DIRECTORY is None:
     raise Exception("Please set the PERSIST_DIRECTORY environment variable")
 
